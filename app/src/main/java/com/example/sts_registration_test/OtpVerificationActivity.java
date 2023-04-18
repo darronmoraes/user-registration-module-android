7d6412d005850f654c4e4d21665d0f1631ec6c9a
Darron Tacio Moraes <111967762+darronmoraes@users.noreply.github.com>
2023-04-18 19:59:18 +0530

    added otp expiration timer

--- app/src/main/java/com/example/sts_registration_test/OtpVerificationActivity.java
+++ app/src/main/java/com/example/sts_registration_test/OtpVerificationActivity.java
@@ -6,12 +6,16 @@ import androidx.appcompat.widget.AppCompatEditText;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.sts_registration_test.otp.verify.OtpVerifyRequest;
 import com.example.sts_registration_test.otp.verify.OtpVerifyResponse;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.Objects;
 
 import retrofit2.Call;
@@ -23,6 +27,8 @@ public class OtpVerificationActivity extends AppCompatActivity {
     AppCompatEditText otpCode;
     AppCompatButton btnVerify;
 
+    TextView timer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +36,8 @@ public class OtpVerificationActivity extends AppCompatActivity {
 
         otpCode = findViewById(R.id.et_otp);
         btnVerify = findViewById(R.id.btn_verify);
+
+        timer = findViewById(R.id.tv_timer);
 
         btnVerify.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,5 +76,28 @@ public class OtpVerificationActivity extends AppCompatActivity {
     public String getEmailFromIntent() {
         Intent intent = getIntent();
         return intent.getStringExtra("email");
+    }
+
+    // otp timer
+    public void otpTimer() {
+        final int SET_SECONDS_FOR_EXPIRATION = 60000;
+        final int COUNT_DOWN_INTERVAL = 1000;
+        final String END_TIMER_VALUE = "00";
+
+        new CountDownTimer(SET_SECONDS_FOR_EXPIRATION, COUNT_DOWN_INTERVAL) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                // to format digit in 2 digits only
+                NumberFormat format = new DecimalFormat("00");
+                long sec = (millisUntilFinished / 1000) % 60;
+
+                timer.setText(String.format("OTP will expire in %s", format.format(sec)));
+            }
+
+            @Override
+            public void onFinish() {
+                timer.setText(END_TIMER_VALUE);
+            }
+        }.start();
     }
 }
